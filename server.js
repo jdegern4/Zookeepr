0191302cd496372737264c7bbf2f2c81a096afc1
Jordan D <degco44@gmail.com>
2022-07-17 13:39:27 -0500

    update request functions

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { animals } = require('./data/animals.json');
 const express = require('express');
-const PORT = process.env.PORT || 301;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -42,12 +42,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 };
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
